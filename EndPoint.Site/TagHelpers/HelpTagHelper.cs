ecccbc0fa8336eaa3da435e28054bcfb6b23ae25
ali2060344931 <alifirozjaii@gmail.com>
2026-06-16 11:20:14 +0330

    CH14050326_2

--- EndPoint.Site/TagHelpers/HelpTagHelper.cs
+++ EndPoint.Site/TagHelpers/HelpTagHelper.cs
@@ -11,16 +11,46 @@ namespace EndPoint.Site.TagHelpers
         {
             output.TagName = "i";
 
-            output.Attributes.SetAttribute("class",
-                "fa fa-question-circle help-icon");
+            output.Attributes.SetAttribute("class", "fa fa-question-circle help-icon");
 
-            output.Attributes.SetAttribute("onclick",
-                $"openHelp('{Key}')");
+            output.Attributes.SetAttribute("onclick", $"openHelp('{Key}')");
 
-            output.Attributes.SetAttribute("title",
-                "راهنما");
+            output.Attributes.SetAttribute("data-bs-toggle", "tooltip");
+            output.Attributes.SetAttribute("data-bs-placement", "top");
+
+            output.Attributes.SetAttribute("title", $"راهنمـــا \n HK: {Key}");
 
             output.TagMode = TagMode.StartTagAndEndTag;
         }
     }
 }
+
+
+//using Microsoft.AspNetCore.Razor.TagHelpers;
+
+//namespace EndPoint.Site.TagHelpers
+//{
+//    [HtmlTargetElement("help", Attributes = "key")]
+//    public class HelpTagHelper : TagHelper
+//    {
+//        public string Key { get; set; }
+
+//        public override void Process(TagHelperContext context, TagHelperOutput output)
+//        {
+//            output.TagName = "i";
+
+//            output.Attributes.SetAttribute("class",
+//                "fa fa-question-circle help-icon");
+
+//            output.Attributes.SetAttribute("onclick",
+//                $"openHelp('{Key}')");
+
+//            // نمایش کلید در Tooltip
+//            output.Attributes.SetAttribute("title",
+//                $"راهنما ({Key})");
+
+//            output.TagMode = TagMode.StartTagAndEndTag;
+//        }
+//    }
+//}
+
