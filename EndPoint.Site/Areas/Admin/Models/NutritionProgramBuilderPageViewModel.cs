b3602e7f4fe557108174bcad7162dc888d6c88d0
Asus <Asus@DESKTOP-9LOOP8H>
2026-06-18 05:36:13 +0330

    CH14050328_1

--- EndPoint.Site/Areas/Admin/Models/NutritionProgramBuilderPageViewModel.cs
+++ EndPoint.Site/Areas/Admin/Models/NutritionProgramBuilderPageViewModel.cs
@@ -12,23 +12,4 @@ namespace EndPoint.Site.Areas.Admin.Models
 
         public BuilderLookupDto Lookups { get; set; } = new();
     }
-    /*
-    public class NutritionProgramBuilderPageViewModel
-    {
-        public ProgramBuilderDto Program { get; set; } = new();
-        public NutritionProgramBuilderLookupsVm Lookups { get; set; } = new();
-    }
-
-    public class NutritionProgramBuilderLookupsVm
-    {
-        public List<LookupItemVm> Foods { get; set; } = new();
-        public List<LookupItemVm> Units { get; set; } = new();
-    }
-
-    public class LookupItemVm
-    {
-        public long Id { get; set; }
-        public string Title { get; set; }
-    }
-    */
 }
--- EndPoint.Site/TagHelpers/HelpTagHelper.cs
+++ EndPoint.Site/TagHelpers/HelpTagHelper.cs
@@ -26,31 +26,3 @@ namespace EndPoint.Site.TagHelpers
 }
 
 
-//using Microsoft.AspNetCore.Razor.TagHelpers;
-
-//namespace EndPoint.Site.TagHelpers
-//{
-//    [HtmlTargetElement("help", Attributes = "key")]
-//    public class HelpTagHelper : TagHelper
-//    {
-//        public string Key { get; set; }
-
-//        public override void Process(TagHelperContext context, TagHelperOutput output)
-//        {
-//            output.TagName = "i";
-
-//            output.Attributes.SetAttribute("class",
-//                "fa fa-question-circle help-icon");
-
-//            output.Attributes.SetAttribute("onclick",
-//                $"openHelp('{Key}')");
-
-//            // نمایش کلید در Tooltip
-//            output.Attributes.SetAttribute("title",
-//                $"راهنما ({Key})");
-
-//            output.TagMode = TagMode.StartTagAndEndTag;
-//        }
-//    }
-//}
-
